fc5f8f08985e825106728aac55ed6ea62da1332d
nizw <612133@nd.com>
2019-08-09 20:36:18 +0800

    双端联系

---
+++ Client/Client/Client/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client {
+    public partial class Form1 : Form {
+        private bool isRun = true;
+        private bool isShow = false;
+        private IPEndPoint ipe;
+        private Socket client_socket;
+        private string resiveMesg = "";
+
+        private int testInt = 0;
+        public Form1() {
+            InitializeComponent();
+
+            string ipStr = "127.0.0.1";
+            int port = 8888;
+            IPAddress ip = IPAddress.Parse(ipStr);
+            ipe = new IPEndPoint(ip, port);
+            Thread mythread = new Thread(MyThread_showMesg);
+            mythread.Start();
+            //创建客户端socket
+            client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //连接服务器
+            try {
+                client_socket.Connect(ipe);
+                Thread thread = new Thread(MyThread_recive);
+                thread.Start();
+            } catch (Exception) {
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Client_SendMesg("I am Big Bee");
+
+        }
+        private void Client_SendMesg(string str) {
+            //发送消息
+            string sendStr = str;
+            byte[] sendBytes = Encoding.ASCII.GetBytes(sendStr);
+            client_socket.Close();
+            
+            try {
+                client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                client_socket.Connect(ipe);
+                client_socket.Send(sendBytes);
+            } catch (Exception) {
+                textBox1.Text = "Failure";
+            }   
+        }
+
+        private void MyThread_recive() {
+            while (isRun) {
+                try {
+                    //接收消息
+                    string recStr = "";
+                    byte[] recBytes = new byte[1024];
+                    int bytes = client_socket.Receive(recBytes, recBytes.Length, 0);
+                    recStr += Encoding.Unicode.GetString(recBytes, 0, bytes);
+                    resiveMesg = "From Server messages: {0}" + recStr;
+                    isShow = true;
+                } catch (Exception) {
+
+                }
+            }
+        }
+
+        private void MyThread_showMesg() {
+            while (true) {
+                while (isShow) {
+                    //testInt++;
+                    //label1.Text = testInt + " :";
+                    label1.Text = resiveMesg;
+                    Thread.Sleep(5000);
+                    label1.Text = "";
+                    resiveMesg = "";
+                    isShow = false;
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e) {
+                isShow = true;
+        }
+
+    }
+}
---
+++ Client/Server2/Server2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server2 {
+    class Program {
+        //private Dictionary<int,s>
+
+        private static Socket client_socket;
+        private static Socket server_socket;
+        private static bool isRun = true;
+        static void Main(string[] args) {
+            //配置端口
+            int port = 8888;//端口
+            string ipStr = "127.0.0.1";
+            IPAddress ip = IPAddress.Parse(ipStr);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+            //创建socket
+            server_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //绑定端口
+            server_socket.Bind(ipe);
+            Console.WriteLine("=====绑定端口{0}=====", ipe);
+            //开始监听
+            server_socket.Listen(10);//设置最大监听数为1
+            Console.WriteLine("=====Server Start and Waitting connection=====");
+            Thread thread = new Thread(Mythread);
+            thread.Start();
+            while (true) {
+                try {
+                    string strInput = Console.ReadLine();
+                    byte[] sendBytes = Encoding.Unicode.GetBytes(strInput);
+                    client_socket.Send(sendBytes);
+                    Console.WriteLine("======向{0}发送一条消息:{1}======", "ta", strInput);
+                } catch (Exception) {
+
+                    throw;
+                }
+                
+            }
+        }
+
+        private static void Mythread() {
+            while (isRun) {
+                try {
+                    client_socket = server_socket.Accept();
+
+                    //接收来自客户端的消息
+                    string name = "";
+                    byte[] recbyte = new byte[1024];
+                    int bytes = client_socket.Receive(recbyte, recbyte.Length, 0);//字节流 
+                    name += Encoding.ASCII.GetString(recbyte, 0, bytes);          //--> 字符流
+                    Console.WriteLine("====={0} 连接成功=====", name);
+                } catch (Exception) {
+
+                }
+            }
+            
+        }
+    }
+}
